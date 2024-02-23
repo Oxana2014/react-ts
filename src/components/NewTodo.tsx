02369fdb3ecab671393afae5dbf3dad893be283f
Oksana Kormilina <keysb@mail.ru>
2024-02-23 18:36:05 +0400

    Add styling

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,25 +1,24 @@
-import React,{useRef} from "react";
+import React, { useRef } from "react";
 
-const NewTodo: React.FC<{onAddTodo: (text: string) => void}> = (props) => {
-const textInputRef = useRef<HTMLInputElement>(null)
+import classes from './NewTodo.module.css'
+
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  const textInputRef = useRef<HTMLInputElement>(null);
 
   const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault()
-    const enteredText = textInputRef.current!.value  // instead of current? if you 100% sure that connection will be established in this point of time 
+    event.preventDefault();
+    const enteredText = textInputRef.current!.value; // instead of current? if you 100% sure that connection will be established in this point of time
 
-    if(enteredText.trim() === '') {
-        // throw an error
-        return;
+    if (enteredText.trim() === "") {
+      // throw an error
+      return;
     }
 
-props.onAddTodo(enteredText)
-
-};
-
-  
+    props.onAddTodo(enteredText);
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className={classes.form}>
       <label htmlFor="text">Todo text</label>
       <input ref={textInputRef} type="text" id="text" />
       <button>Add Todo</button>
