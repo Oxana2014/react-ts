d4563992945f8c00d2a219b04eaefc406efb2dd0
Oksana Kormilina <keysb@mail.ru>
2024-02-23 18:27:55 +0400

    Add adding new Todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import Todos from './components/Todos';
-import Todo from './models/todo';
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
+import NewTodo from "./components/NewTodo";
+import { useState } from "react";
 
-const todos = [
-  new Todo( 'Learn React'),
-  new Todo('learn TypeScript')
-]
+// const todos = [
+//   new Todo( 'Learn React'),
+//   new Todo('learn TypeScript')
+// ]
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  function addTodoHandler(text: string) {
+    const newTodo = new Todo(text);
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  }
+
   return (
-    <div >
-    <Todos items={todos}/>
+    <div>
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} />
     </div>
   );
 }
---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,29 @@
+import React,{useRef} from "react";
+
+const NewTodo: React.FC<{onAddTodo: (text: string) => void}> = (props) => {
+const textInputRef = useRef<HTMLInputElement>(null)
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault()
+    const enteredText = textInputRef.current!.value  // instead of current? if you 100% sure that connection will be established in this point of time 
+
+    if(enteredText.trim() === '') {
+        // throw an error
+        return;
+    }
+
+props.onAddTodo(enteredText)
+
+};
+
+  
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="text">Todo text</label>
+      <input ref={textInputRef} type="text" id="text" />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+export default NewTodo;
